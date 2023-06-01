b4a8e5a6927caeb3f0058cc323425400de7f486e
marchukmx <mmmarch1k@gmail.com>
2023-06-01 20:28:51 +0300

    #8

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-#1
+#8
+def filter_capitalized_strings(strings):
+    capitalized_strings = [string for string in strings if string[0].isupper()]
+    return capitalized_strings
 
-print("Привіт, світ!")
-
+vvod_strings = ["Салам", "Алейкум", "діма", "магазин" , "Вова" , "Лох"]
+filtered_strings = filter_capitalized_strings(vvod_strings)
+print("Результат:", filtered_strings)
