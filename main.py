7829403a4b2aef30c498762099d6bf4504f5f862
marchukmx <mmmarch1k@gmail.com>
2023-06-01 19:58:41 +0300

    #6

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-#1
+#6
 
-print("Привіт, світ!")
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+parni = []
+
+for i in numbers:
+    if i % 2 == 0:
+        parni.append(i)
+
+print("Список парних чисел:", parni)
 
