d1db2ff8de7b4b74f25d1a6c4a9b379652c54b52
marchukmx <mmmarch1k@gmail.com>
2023-06-01 20:41:53 +0300

    №3  частина 2

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-#1
+#3 part 2
 
-print("Привіт, світ!")
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
 
+    def get_info(self):
+        print("Назва книги:", self.title)
+        print("Автор:", self.author)
+
+book1 = Book("Война и Мир", "Лев Тостой")
+book1.get_info()
+
+book2 = Book("Пайтон це легко", "Дмитро Кучинський")
+book2.get_info()
