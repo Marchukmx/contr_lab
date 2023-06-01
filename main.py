2a21a5b8bab7d7f4c2cd4f50faac5a292294c3f2
marchukmx <mmmarch1k@gmail.com>
2023-06-01 20:46:09 +0300

    #7

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-#1
+#7
 
-print("Привіт, світ!")
-
+student_grades = {}
+def create_student_grades():
+    while True:
+        student_name = input("Введіть ім'я студента (або введіть 'кінець' для завершення рахування студентів): ")
+        if student_name == 'кінець':
+            break
+        grade = input("Введіть оцінку студента {}: ".format(student_name))
+        student_grades[student_name] = int(grade)
+def find_student_with_highest_grade():
+    highest_grade = max(student_grades.values())
+    for student, grade in student_grades.items():
+        if grade == highest_grade:
+            return student
+create_student_grades()
+student_with_highest_grade = find_student_with_highest_grade()
+print("Студент з найвищою оцінкою: ", student_with_highest_grade)
