1bc57035014273040b3ca673091d68937cd9116b
marchukmx <mmmarch1k@gmail.com>
2023-06-01 20:44:22 +0300

    №4  частина 2

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-#1
+#4 part 2
 
-print("Привіт, світ!")
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
 
+    def make_sound(self):
+        print(f"{self.name} видає звук: {self.sound}")
+
+animal1 = Animal("Котяра", "Мяу")
+animal1.make_sound()
+
+animal2 = Animal("Собака", "Гав")
+animal2.make_sound()
+
+animal3 = Animal("Глеб", "Дайте кристалик")
+animal3.make_sound()
