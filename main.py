18c7098acad540e5d91e0b279e0c9f09b605ab70
marchukmx <mmmarch1k@gmail.com>
2023-06-01 20:34:36 +0300

    №1  частина 2

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-#1
+#1 part 2
 
-print("Привіт, світ!")
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+    def get_info(self):
+        print("Ім'я студента:", self.name)
+        print("Вік студента:", self.age)
+
+student1 = Student("Дімончик", 14)
+student1.get_info()
+
+student2 = Student("Вовчик", 14)
+student2.get_info()
+student3 = Student("Максим", 16)
+student3.get_info()
