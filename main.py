be1146f777c3e4bb61ff37cde1580246c55da188
marchukmx <mmmarch1k@gmail.com>
2023-06-01 20:31:54 +0300

    #9

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-#1
+#9
 
-print("Привіт, світ!")
+def filter_python_strings(strings):
+    python_strings = [i for i in strings if "Python" in i]
+    return python_strings
 
+user_strings = ["петух", "world", "Python легче C++", "вивчаю Python"]
+gotovi = filter_python_strings(user_strings)
+print("Результат:", gotovi)
