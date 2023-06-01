b32954e169f1ff27a64a35f1e2dbdad222190588
marchukmx <mmmarch1k@gmail.com>
2023-06-01 19:46:18 +0300

    #2

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-#1
+#2
+start = int(input("Введіть початкове число: "))
+end = int(input("Введіть кінцеве число: "))
 
-print("Привіт, світ!")
+if start > end:
+    start, end = end, start
 
+summa = 0
+for i in range(start, end + 1):
+    summa += i
+
+print("Сума чисел у діапазоні від", start, "до", end, "дорівнює", summa)
